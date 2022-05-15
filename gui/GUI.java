519471498d1768cfc05f717caa7d3d195ac88db6
Kuba Bigaj <k.bigaj666@gmail.com>
2022-05-15 21:50:46 +0200

    Added adn updated documentation and gui. package skeleton

---
+++ gui/GUI.java
@@ -0,0 +1,22 @@
+package gui;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GUI {
+    private boolean isPaused;
+
+    static class PauseListener implements KeyListener {
+        public void keyTyped(KeyEvent k) {
+
+        }
+
+        public void keyPressed(KeyEvent k) {
+
+        }
+
+        public void keyReleased(KeyEvent k) {
+            
+        }
+    }
+}
