a9c58ed5456cca946cd472749c2faee07702aadc
Kuba Bigaj <k.bigaj666@gmail.com>
2022-05-15 21:13:21 +0200

    Clean slate

--- test.java
+++
@@ -1,7 +0,0 @@
-import test_package.simpleClass;
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Test message");
-        simpleClass.write();
-    }
-}
--- test_package/simpleClass.java
+++
@@ -1,7 +0,0 @@
-package test_package;
-
-public class simpleClass {
-    public static void write() {
-        System.out.println("bagno");
-    }
-}
