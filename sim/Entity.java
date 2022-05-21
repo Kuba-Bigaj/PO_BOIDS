f0eebed76b698c5d528ad0b0973aa2aa6d039053
Kuba Bigaj <k.bigaj666@gmail.com>
2022-05-21 20:38:18 +0200

    Compatibility changes

--- sim/Entity.java
+++ sim/Entity.java
@@ -1,18 +1,24 @@
 package sim;
+import gui.GUIable;
+
+import java.awt.*;
 import java.util.ArrayList;
 
-abstract class Entity {
-    Double posX,posY,velocity=0.00,direction=0.00,turnRate=0.00,mass,massDecay;
+abstract class Entity implements GUIable {
+    Double posX, posY, velocity = 0.00, direction = 0.00, turnRate = 0.00, mass, massDecay;
 
-    Entity(Double posX,Double posY,Double mass,Double massDecay)
-    {
-        this.posX=posX;
-        this.posY=posY;
-        this.mass=mass;
-        this.massDecay=massDecay;
+    Entity(Double posX, Double posY, Double mass, Double massDecay) {
+        this.posX = posX;
+        this.posY = posY;
+        this.mass = mass;
+        this.massDecay = massDecay;
     }
-    public void die(ArrayList<Entity> entities)
-    {
+
+    public void die(ArrayList<Entity> entities) {
+
+    }
+
+    public void draw(Graphics2D pen) {
 
     }
 }
