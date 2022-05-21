ba7ec579505b8baba6e1dba8bbd27f2e22eb60a3
PCyganiuk <pcyganiuk@gmail.com>
2022-05-21 20:20:18 +0200

    klasa Food

--- sim/Entity.java
+++ sim/Entity.java
@@ -1,4 +1,18 @@
 package sim;
+import java.util.ArrayList;
 
-public class Entity {
+abstract class Entity {
+    Double posX,posY,velocity=0.00,direction=0.00,turnRate=0.00,mass,massDecay;
+
+    Entity(Double posX,Double posY,Double mass,Double massDecay)
+    {
+        this.posX=posX;
+        this.posY=posY;
+        this.mass=mass;
+        this.massDecay=massDecay;
+    }
+    public void die(ArrayList<Entity> entities)
+    {
+
+    }
 }
---
+++ sim/Food.java
@@ -0,0 +1,7 @@
+package sim;
+
+public class Food extends Entity {
+    Food(Double posX,Double posY,Double mass,Double massDecay) {
+        super(posX, posY, mass, massDecay);
+    }
+}
