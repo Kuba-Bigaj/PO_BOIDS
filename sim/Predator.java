b2aded08d67d8e5ae10711c03f26026c67e469df
Kuba Bigaj <k.bigaj666@gmail.com>
2022-06-12 22:00:44 +0200

    Reformatted files, fixed prey not referencing appropriate values

--- sim/Predator.java
+++ sim/Predator.java
@@ -7,120 +7,116 @@ import static java.lang.Math.*;
 
 public class Predator extends Entity {
 
-    protected Integer fovPrey;
-    protected Double desiredSeparation;
-    Predator(Double posX, Double posY, Double mass, Double massDecay,Double dir,Double turnRate,Integer fovPrey,Double desiredSeparation, Simulation sim){
-        super(posX,posY,mass,massDecay, sim);
-        this.vel=5.0;
-        this.dir=toRadians(dir);
-        this.turnRate=turnRate;
-        this.fovPrey=fovPrey;
-        this.desiredSeparation=desiredSeparation;
-    }
-    double separation() {
-        Vector avgPos = new Vector(0, 0);
-        Vector diff = new Vector(0, 0);
-        int total = 0, i;
-        double d, a, b, avgDir = 0.0;
-        for (i = 0; i < this.sim.entities.size(); i++) {
-            d = Math.sqrt(((this.posX - this.sim.entities.get(i).posX) * (this.posX - this.sim.entities.get(i).posX)) + ((this.posY - this.sim.entities.get(i).posY) * (this.posY - this.sim.entities.get(i).posY)));
-            if (this.sim.entities.get(i) != this && d < desiredSeparation  && this.sim.entities.get(i).getClass().getName().equals("sim.Predator")) {
-                diff.x = posX - this.sim.entities.get(i).posX;
-                diff.y = posY - this.sim.entities.get(i).posY;
-                diff.x /= d;
-                diff.y /= d;
-                avgPos.x += posX + diff.x;
-                avgPos.y += posY + diff.y;
-                total++;
-            }
-        }
-        if (total > 0) {
-            avgPos.x /= total;
-            avgPos.y /= total;
-            a = avgPos.x - posX;
-            b = avgPos.y - posY;
-            if(a>0)
-                avgDir=atan(b/a);
-            else if(a<0)
-                avgDir=atan((b/a))+Math.PI;
-            avgDir-=dir;
+	protected Integer fovPrey;
+	protected Double desiredSeparation;
 
-            if(avgDir>turnRate)
-                avgDir=turnRate;
-            else if(avgDir<-turnRate)
-                avgDir = -turnRate;
+	Predator(Double posX, Double posY, Double mass, Double massDecay, Double dir, Double turnRate, Integer fovPrey, Double desiredSeparation, Simulation sim) {
+		super(posX, posY, mass, massDecay, sim);
+		this.vel = 5.0;
+		this.dir = toRadians(dir);
+		this.turnRate = turnRate;
+		this.fovPrey = fovPrey;
+		this.desiredSeparation = desiredSeparation;
+	}
 
-        }
+	@Override
+	public void move() {
+		double sep = separation(), avgDir = 0;
+		int div = 1;
+		if (sep != 0) div++;
+		avgDir += eat() + sep;
+		avgDir /= div;
+		dir += avgDir;
+		posX += cos(dir) * vel;
+		posY += sin(dir) * vel;
+		breed();
+		mass -= massDecay;
+		if (this.mass < 0) {
+			this.die();
+			System.out.println("Predator starved!");
+		}
+	}
+
+	void breed() {
+		Random rand = new Random();
+		if (this.mass >= 4) {
+			this.sim.add(new Predator(this.posX, this.posY, 1.0, this.massDecay, rand.nextDouble() * Math.PI * 2, Math.PI / 4, this.fovPrey, this.desiredSeparation, this.sim));
+			this.mass -= 1;
+		}
+	}
+
+	double eat() {
+
+		int i, id = -1;
+		double d, minD = fovPrey + 1.0, a, b, avgDir = 0.0;
+		Vector avgPos = new Vector(0, 0);
+
+		if (this.mass < 3) {
+			for (i = 0; i < sim.entities.size(); i++) {
+				d = Math.sqrt(Math.pow(this.posX - sim.entities.get(i).posX, 2) + Math.pow(this.posY - sim.entities.get(i).posY, 2));
+				if (sim.entities.get(i) != this && d < fovPrey && sim.entities.get(i).getClass().getName().equals("sim.Prey")) {
+					if (d < minD) {
+						minD = d;
+						id = i;
+					}
+				}
+			}
+			if (id >= 0) {
+				if (this.posX - sim.entities.get(id).posX < 5 && this.posY - sim.entities.get(id).posY < 5) {
+					this.mass += sim.entities.get(id).mass;
+					this.sim.entities.get(id).die();
+					System.out.println("Prey slaughtered!");
+				} else {
+					avgPos.x = sim.entities.get(id).posX;
+					avgPos.y = sim.entities.get(id).posY;
+					a = avgPos.x - posX;
+					b = avgPos.y - posY;
+					if (a > 0) avgDir = atan(b / a);
+					else if (a < 0) avgDir = atan((b / a)) + Math.PI;
+					avgDir -= dir;
+
+					if (avgDir > turnRate) avgDir = turnRate;
+					else if (avgDir < -turnRate) avgDir = -turnRate;
+				}
+			}
+		}
+		return avgDir;
+
+	}
+
+	double separation() {
+		Vector avgPos = new Vector(0, 0);
+		Vector diff = new Vector(0, 0);
+		int total = 0, i;
+		double d, a, b, avgDir = 0.0;
+		for (i = 0; i < this.sim.entities.size(); i++) {
+			d = Math.sqrt(((this.posX - this.sim.entities.get(i).posX) * (this.posX - this.sim.entities.get(i).posX)) + ((this.posY - this.sim.entities.get(i).posY) * (this.posY - this.sim.entities.get(i).posY)));
+			if (this.sim.entities.get(i) != this && d < desiredSeparation && this.sim.entities.get(i).getClass().getName().equals("sim.Predator")) {
+				diff.x = posX - this.sim.entities.get(i).posX;
+				diff.y = posY - this.sim.entities.get(i).posY;
+				diff.x /= d;
+				diff.y /= d;
+				avgPos.x += posX + diff.x;
+				avgPos.y += posY + diff.y;
+				total++;
+			}
+		}
+		if (total > 0) {
+			avgPos.x /= total;
+			avgPos.y /= total;
+			a = avgPos.x - posX;
+			b = avgPos.y - posY;
+			if (a > 0) avgDir = atan(b / a);
+			else if (a < 0) avgDir = atan((b / a)) + Math.PI;
+			avgDir -= dir;
+
+			if (avgDir > turnRate) avgDir = turnRate;
+			else if (avgDir < -turnRate) avgDir = -turnRate;
+
+		}
 
 
-        return avgDir;
-    }
-    double eat(){
-
-        int i,id=-1;
-        double d,minD=fovPrey+1.0,a,b,avgDir=0.0;
-        Vector avgPos=new Vector(0,0);
-
-        if(this.mass<3){
-        for(i=0;i<sim.entities.size();i++) {
-            d = Math.sqrt(Math.pow(this.posX - sim.entities.get(i).posX, 2) + Math.pow(this.posY - sim.entities.get(i).posY, 2));
-            if (sim.entities.get(i) != this && d < fovPrey  && sim.entities.get(i).getClass().getName().equals("sim.Prey")) {
-                if (d < minD) {
-                    minD = d;
-                    id = i;
-                }
-            }
-        }
-        if(id>=0) {
-            if (this.posX - sim.entities.get(id).posX < 1 && this.posY - sim.entities.get(id).posY < 1) {
-                this.mass += sim.entities.get(id).mass;
-                this.sim.entities.get(id).die();
-                System.out.println("Prey slaughtered!");
-            }
-            else {
-                avgPos.x = sim.entities.get(id).posX;
-                avgPos.y = sim.entities.get(id).posY;
-                a = avgPos.x - posX;
-                b = avgPos.y - posY;
-                if (a > 0)
-                    avgDir = atan(b / a);
-                else if (a < 0)
-                    avgDir = atan((b / a)) + Math.PI;
-                avgDir -= dir;
-
-                if (avgDir > turnRate)
-                    avgDir = turnRate;
-                else if (avgDir < -turnRate)
-                    avgDir = -turnRate;
-            }
-        }}
-        return avgDir;
-
-    }
-    void breed(){
-        Random rand = new Random();
-        if (this.mass >= 4) {
-            this.sim.add(new Predator(this.posX, this.posY, 1.0, this.massDecay, rand.nextDouble() *Math.PI*2, Math.PI / 4,this.fovPrey,this.desiredSeparation,this.sim));
-            this.mass -= 1;
-        }
-    }
-    @Override
-    public void move(){
-        double sep=separation(),avgDir=0;
-        int div=1;
-        if(sep!=0)
-            div++;
-        avgDir+=eat()+sep;
-        avgDir/=div;
-        dir+=avgDir;
-        posX+= cos(dir)*vel;
-        posY+= sin(dir)*vel;
-        breed();
-        mass-=massDecay;
-        if (this.mass<0){
-            this.die();
-            System.out.println("Predator starved!");
-        }
-    }
+		return avgDir;
+	}
 
 }
--- sim/Prey.java
+++ sim/Prey.java
@@ -10,286 +10,251 @@ import static java.lang.Math.*;
  *
  * @author Paweł Cyganiuk
  */
-public class Prey extends Entity{
-    protected Integer fovAlly;
-    protected Integer fovEnemy;
-    protected Integer fovFood;
-    protected Double desiredSeparation;
-    /**
-     * constructor Prey extends Entity
-     */
-    Prey(Double posX, Double posY, Double mass, Double massDecay, Double dir, Double turnRate, Integer fovAlly, Integer fovEnemy, Integer fovFood, Double desiredSeparation, Simulation sim) {
-        super(posX, posY, mass, massDecay, sim);
-        this.vel = 2.0;
-        this.dir = toRadians(dir);
-        this.turnRate = turnRate;
-        this.fovAlly = fovAlly;
-        this.fovEnemy = fovEnemy;
-        this.fovFood = fovFood;
-        this.desiredSeparation = desiredSeparation;
-    }
+public class Prey extends Entity {
+	protected Integer fovAlly;
+	protected Integer fovEnemy;
+	protected Integer fovFood;
+	protected Double desiredSeparation;
 
-    /**
-     * Method Alignment is used to steer entities towards average direction of the group
-     *
-     *
-     */
-    double alignment() {
-        double avgDir = 0.0, d;
-        int i, total = 0;
+	/**
+	 * constructor Prey extends Entity
+	 */
+	Prey(Double posX, Double posY, Double mass, Double massDecay, Double dir, Double turnRate, Integer fovAlly, Integer fovEnemy, Integer fovFood, Double desiredSeparation, Simulation sim) {
+		super(posX, posY, mass, massDecay, sim);
+		this.vel = 2.0;
+		this.dir = toRadians(dir);
+		this.turnRate = turnRate;
+		this.fovAlly = fovAlly;
+		this.fovEnemy = fovEnemy;
+		this.fovFood = fovFood;
+		this.desiredSeparation = desiredSeparation;
+	}
 
-        for (i = 0; i < this.sim.entities.size(); i++) {
-            d = Math.sqrt(((this.posX - this.sim.entities.get(i).posX) * (this.posX - this.sim.entities.get(i).posX)) + ((this.posY - this.sim.entities.get(i).posY) * (this.posY - this.sim.entities.get(i).posY)));
-            if (this.sim.entities.get(i) != this && d < fovAlly && this.sim.entities.get(i).getClass().getName().equals("sim.Prey")) {
-                avgDir += this.sim.entities.get(i).dir;
-                total++;
-            }
-        }
-        if(total>0){
-            avgDir/=total;
-        }
-        avgDir-=this.dir;
-        if(avgDir>turnRate)
-            avgDir = turnRate;
-        else if (avgDir < -turnRate)
-            avgDir = -turnRate;
-        return avgDir;
-    }
+	/**
+	 * This method is responsible for displaying this object
+	 *
+	 * @param pen A Graphics2D object that is used to draw this entity.
+	 * @author Kuba Bigaj
+	 */
+	@Override
+	public void draw(Graphics2D pen) {
+		pen.translate(this.posX.intValue(), this.posY.intValue());
+		pen.setPaint(Color.RED);
+		pen.drawRoundRect(0, 0, 5, 5, 3, 3);
+		pen.translate(-this.posX.intValue(), -this.posY.intValue());
+	}
 
-    /**
-     * Method Cohesion is used to steer entities towards average location of the group
-     *
-     *
-     */
-    double cohesion() {
-        int i, total = 0;
-        double a, b, d, avgDir = 0.0;
-        Vector avgPos = new Vector(0, 0);
-        for (i = 0; i < this.sim.entities.size(); i++) {
-            d = Math.sqrt(((this.posX - this.sim.entities.get(i).posX) * (this.posX - this.sim.entities.get(i).posX)) + ((this.posY - this.sim.entities.get(i).posY) * (this.posY - this.sim.entities.get(i).posY)));
-            if (this.sim.entities.get(i) != this && d < fovAlly && this.sim.entities.get(i).getClass().getName().equals("sim.Prey")) {
-                avgPos.x += this.sim.entities.get(i).posX;
-                avgPos.y += this.sim.entities.get(i).posY;
-                total++;
-            }
-        }
-        if (total > 0) {
-            avgPos.x /= total;
-            avgPos.y /= total;
-            a = avgPos.x - this.posX;
-            b = avgPos.y - this.posY;
-            if (a > 0)
-                avgDir = atan(b/a);
-            else if(a<0)
-                avgDir=atan((b/a))+Math.PI;
-            avgDir-=dir;
+	/**
+	 * Method Move is used to add all movements of the object and turn it into one vector
+	 */
 
-            if(avgDir>turnRate)
-                avgDir=turnRate;
-            else if(avgDir<-turnRate)
-                avgDir = -turnRate;
+	@Override
+	public void move() {
+		double avgDir, hunger = 1 + (5 / mass), runDiv, eatDiv, algnDiv, cohDiv, sepDiv;
+		int div = 0;
+		algnDiv = alignment();
+		cohDiv = cohesion();
+		sepDiv = separation();
+		runDiv = run();
+		eatDiv = eat();
+		if (algnDiv != 0) div++;
+		if (cohDiv != 0) div++;
+		if (sepDiv != 0) div += 100;
+		if (runDiv != 0) div++;
+		if (eatDiv != 0) div += hunger;
+		if (div > 0) {
+			avgDir = algnDiv + cohDiv + 100 * sepDiv + hunger * eatDiv + runDiv;
+			avgDir /= div;
+			dir += avgDir;
+		}
+		posX += cos(dir) * vel;
+		posY += sin(dir) * vel;
+		breed();
+		mass -= massDecay;
+		if (mass < 0) {
+			this.die();
+			System.out.println("Prey starved!");
+		}
+	}
 
-        }
-        return avgDir;
+	/**
+	 * Method Alignment is used to steer entities towards average direction of the group
+	 */
+	double alignment() {
+		double avgDir = 0.0, d;
+		int i, total = 0;
 
-    }
+		for (i = 0; i < this.sim.entities.size(); i++) {
+			d = Math.sqrt(((this.posX - this.sim.entities.get(i).posX) * (this.posX - this.sim.entities.get(i).posX)) + ((this.posY - this.sim.entities.get(i).posY) * (this.posY - this.sim.entities.get(i).posY)));
+			if (this.sim.entities.get(i) != this && d < fovAlly && this.sim.entities.get(i).getClass().getName().equals("sim.Prey")) {
+				avgDir += this.sim.entities.get(i).dir;
+				total++;
+			}
+		}
+		if (total > 0) {
+			avgDir /= total;
+		}
+		avgDir -= this.dir;
+		if (avgDir > turnRate) avgDir = turnRate;
+		else if (avgDir < -turnRate) avgDir = -turnRate;
+		return avgDir;
+	}
 
-    /**
-     * Method Separation is used to steer entities to the opposite direction than the average location of the group to avoid crowding
-     *
-     *
-     */
-    double separation() {
-        Vector avgPos = new Vector(0, 0);
-        Vector diff = new Vector(0, 0);
-        int total = 0, i;
-        double d, a, b, avgDir = 0.0;
-        for (i = 0; i < this.sim.entities.size(); i++) {
-            d = Math.sqrt(((this.posX - this.sim.entities.get(i).posX) * (this.posX - this.sim.entities.get(i).posX)) + ((this.posY - this.sim.entities.get(i).posY) * (this.posY - this.sim.entities.get(i).posY)));
-            if (this.sim.entities.get(i) != this && d < this.desiredSeparation  && this.sim.entities.get(i).getClass().getName().equals("sim.Prey")) {
-                diff.x = posX - this.sim.entities.get(i).posX;
-                diff.y = posY - this.sim.entities.get(i).posY;
-                diff.x /= d;
-                diff.y /= d;
-                avgPos.x += posX + diff.x;
-                avgPos.y += posY + diff.y;
-                total++;
-            }
-        }
-        if (total > 0) {
-            avgPos.x /= total;
-            avgPos.y /= total;
-            a = avgPos.x - posX;
-            b = avgPos.y - posY;
-            if(a>0)
-                avgDir=atan(b/a);
-            else if(a<0)
-                avgDir=atan((b/a))+Math.PI;
-            avgDir-=dir;
+	/**
+	 * Method Breed is used to add new objects based on the amount of food they ate
+	 */
+	void breed() {
+		Random rand = new Random();
+		if (this.mass >= 4) {
+			this.sim.add(new Prey(this.posX + 1, this.posY + 1, 1.0, this.massDecay, rand.nextDouble() * Math.PI * 2, Math.PI / 4, this.fovAlly, this.fovEnemy, this.fovFood, this.desiredSeparation, this.sim));
+			this.mass -= 1;
+		}
+	}
 
-            if(avgDir>turnRate)
-                avgDir=turnRate;
-            else if(avgDir<-turnRate)
-                avgDir = -turnRate;
+	/**
+	 * Method Cohesion is used to steer entities towards average location of the group
+	 */
+	double cohesion() {
+		int i, total = 0;
+		double a, b, d, avgDir = 0.0;
+		Vector avgPos = new Vector(0, 0);
+		for (i = 0; i < this.sim.entities.size(); i++) {
+			d = Math.sqrt(((this.posX - this.sim.entities.get(i).posX) * (this.posX - this.sim.entities.get(i).posX)) + ((this.posY - this.sim.entities.get(i).posY) * (this.posY - this.sim.entities.get(i).posY)));
+			if (this.sim.entities.get(i) != this && d < fovAlly && this.sim.entities.get(i).getClass().getName().equals("sim.Prey")) {
+				avgPos.x += this.sim.entities.get(i).posX;
+				avgPos.y += this.sim.entities.get(i).posY;
+				total++;
+			}
+		}
+		if (total > 0) {
+			avgPos.x /= total;
+			avgPos.y /= total;
+			a = avgPos.x - this.posX;
+			b = avgPos.y - this.posY;
+			if (a > 0) avgDir = atan(b / a);
+			else if (a < 0) avgDir = atan((b / a)) + Math.PI;
+			avgDir -= dir;
 
-        }
+			if (avgDir > turnRate) avgDir = turnRate;
+			else if (avgDir < -turnRate) avgDir = -turnRate;
 
+		}
+		return avgDir;
 
-        return avgDir;
-    }
+	}
 
-    /**
-     * Method Eat is used to navigate objects towards food
-     *
-     *
-     */
-    double eat() {
-        int i, id = -1;
-        double d, minD = 300 + 1.0, a, b, avgDir = 0.0;
-        Vector avgPos = new Vector(0, 0);
-
-
-        for (i = 0; i < this.sim.entities.size(); i++) {
-
-            d = Math.sqrt(Math.pow(this.posX - this.sim.entities.get(i).posX, 2) + Math.pow(this.posY - this.sim.entities.get(i).posY, 2));
-            if (d < 200 && this.sim.entities.get(i).getClass().getName().equals("sim.Food")) {
-                if (d < minD) {
-                    minD = d;
-                    id = i;
-                }
-            }
-        }
-        if (id >= 0) {
-            if (abs(this.posX - this.sim.entities.get(id).posX) < 5 && abs(this.posY - this.sim.entities.get(id).posY) < 5) {
-                this.mass += this.sim.entities.get(id).mass;
-                this.sim.entities.get(id).die();
-
-            } else {
-                avgPos.x = this.sim.entities.get(id).posX;
-                avgPos.y = this.sim.entities.get(id).posY;
-                a = avgPos.x - posX;
-                b = avgPos.y - posY;
-                if (a > 0)
-                    avgDir = atan(b / a);
-                else if (a < 0)
-                    avgDir = atan(b / a) + Math.PI;
-                avgDir -= dir;
-
-                if (avgDir > turnRate)
-                    avgDir = turnRate;
-                else if (avgDir < -turnRate)
-                    avgDir = -turnRate;
-            }
-        }
-        return avgDir;
-    }
-
-    /**
-     * Method Breed is used to add new objects based on the amount of food they ate
-     */
-    void breed() {
-        Random rand = new Random();
-        if (this.mass >= 4) {
-            this.sim.add(new Prey(this.posX+1, this.posY+1, 1.0, this.massDecay, rand.nextDouble() *Math.PI*2, Math.PI / 4, this.fovAlly, this.fovEnemy, this.fovFood, this.desiredSeparation, this.sim));
-            this.mass -= 1;
-        }
-    }
-
-    /**
-     * Method Run is used to navigate objects to run from the predators
-     *
-     *
-     */
-    double run() {
-        int i, total = 0;
-        double a, b, d, avgDir = 0.0;
-        Vector diff = new Vector(0, 0);
-        Vector avgPos = new Vector(0, 0);
-        for (i = 0; i < this.sim.entities.size(); i++) {
-            d = Math.sqrt(Math.pow(this.posX - this.sim.entities.get(i).posX, 2) + Math.pow(this.posY - this.sim.entities.get(i).posY, 2));
-            if (this.sim.entities.get(i) != this && d < fovEnemy && this.sim.entities.get(i).getClass().getName().equals("sim.Predator")) {
-                diff.x = posX - this.sim.entities.get(i).posX;
-                diff.y = posY - this.sim.entities.get(i).posY;
-                diff.x /= d;
-                diff.y /= d;
-                avgPos.x += posX + diff.x;
-                avgPos.y += posX + diff.y;
-                total++;
-            }
-        }
-        if (total > 0) {
-            avgPos.x /= total;
-            avgPos.y /= total;
-            a = avgPos.x - posX;
-            b = avgPos.y - posY;
-            if(a>0)
-                avgDir=atan(b/a);
-            else if(a<0)
-                avgDir=atan((b/a))+Math.PI;
-            avgDir-=dir;
-
-            if(avgDir>turnRate)
-                avgDir=turnRate;
-            else if(avgDir<-turnRate)
-                avgDir=-turnRate;
+	/**
+	 * Method Eat is used to navigate objects towards food
+	 */
+	double eat() {
+		int i, id = -1;
+		double d, minD = 300 + 1.0, a, b, avgDir = 0.0;
+		Vector avgPos = new Vector(0, 0);
 
 
+		for (i = 0; i < this.sim.entities.size(); i++) {
 
-        }
-        return avgDir;
-    }
+			d = Math.sqrt(Math.pow(this.posX - this.sim.entities.get(i).posX, 2) + Math.pow(this.posY - this.sim.entities.get(i).posY, 2));
+			if (d < this.fovFood && this.sim.entities.get(i).getClass().getName().equals("sim.Food")) {
+				if (d < minD) {
+					minD = d;
+					id = i;
+				}
+			}
+		}
+		if (id >= 0) {
+			if (abs(this.posX - this.sim.entities.get(id).posX) < 4 && abs(this.posY - this.sim.entities.get(id).posY) < 4) {
+				this.mass += this.sim.entities.get(id).mass;
+				this.sim.entities.get(id).die();
 
-    /**
-     * Method Move is used to add all movements of the object and turn it into one vector
-     *
-     *
-     */
+			} else {
+				avgPos.x = this.sim.entities.get(id).posX;
+				avgPos.y = this.sim.entities.get(id).posY;
+				a = avgPos.x - posX;
+				b = avgPos.y - posY;
+				if (a > 0) avgDir = atan(b / a);
+				else if (a < 0) avgDir = atan(b / a) + Math.PI;
+				avgDir -= dir;
 
-    @Override
-    public void move() {
-        double avgDir, hunger=1+(5/mass),runDiv,eatDiv,algnDiv,cohDiv,sepDiv;
-        int div=0;
-        algnDiv=alignment();
-        cohDiv=cohesion();
-        sepDiv=separation();
-        runDiv=run();
-        eatDiv=eat();
-        if(algnDiv!=0)
-            div++;
-        if(cohDiv!=0)
-            div++;
-        if(sepDiv!=0)
-            div+=100;
-        if(runDiv!=0)
-            div++;
-        if(eatDiv!=0)
-            div+=hunger;
-        if(div>0) {
-            avgDir = algnDiv + cohDiv +100* sepDiv + hunger * eatDiv + runDiv;
-            avgDir /= div;
-            dir += avgDir;
-        }
-        posX += cos(dir) * vel;
-        posY += sin(dir) * vel;
-        breed();
-        mass -= massDecay;
-        if (mass<0){
-            this.die();
-            System.out.println("Prey starved!");
-        }
-    }
+				if (avgDir > turnRate) avgDir = turnRate;
+				else if (avgDir < -turnRate) avgDir = -turnRate;
+			}
+		}
+		return avgDir;
+	}
 
-    /**
-     * This method is responsible for displaying this object
-     *
-     * @param pen A Graphics2D object that is used to draw this entity.
-     * @author Kuba Bigaj
-     */
-    @Override
-    public void draw(Graphics2D pen) {
-        pen.translate(this.posX.intValue(), this.posY.intValue());
-        pen.setPaint(Color.RED);
-        pen.drawRoundRect(0, 0, 5, 5, 3, 3);
-        pen.translate(-this.posX.intValue(), -this.posY.intValue());
-    }
+	/**
+	 * Method Run is used to navigate objects to run from the predators
+	 */
+	double run() {
+		int i, total = 0;
+		double a, b, d, avgDir = 0.0;
+		Vector diff = new Vector(0, 0);
+		Vector avgPos = new Vector(0, 0);
+		for (i = 0; i < this.sim.entities.size(); i++) {
+			d = Math.sqrt(Math.pow(this.posX - this.sim.entities.get(i).posX, 2) + Math.pow(this.posY - this.sim.entities.get(i).posY, 2));
+			if (this.sim.entities.get(i) != this && d < fovEnemy && this.sim.entities.get(i).getClass().getName().equals("sim.Predator")) {
+				diff.x = posX - this.sim.entities.get(i).posX;
+				diff.y = posY - this.sim.entities.get(i).posY;
+				diff.x /= d;
+				diff.y /= d;
+				avgPos.x += posX + diff.x;
+				avgPos.y += posX + diff.y;
+				total++;
+			}
+		}
+		if (total > 0) {
+			avgPos.x /= total;
+			avgPos.y /= total;
+			a = avgPos.x - posX;
+			b = avgPos.y - posY;
+			if (a > 0) avgDir = atan(b / a);
+			else if (a < 0) avgDir = atan((b / a)) + Math.PI;
+			avgDir -= dir;
+
+			if (avgDir > turnRate) avgDir = turnRate;
+			else if (avgDir < -turnRate) avgDir = -turnRate;
+
+
+		}
+		return avgDir;
+	}
+
+	/**
+	 * Method Separation is used to steer entities to the opposite direction than the average location of the group to avoid crowding
+	 */
+	double separation() {
+		Vector avgPos = new Vector(0, 0);
+		Vector diff = new Vector(0, 0);
+		int total = 0, i;
+		double d, a, b, avgDir = 0.0;
+		for (i = 0; i < this.sim.entities.size(); i++) {
+			d = Math.sqrt(((this.posX - this.sim.entities.get(i).posX) * (this.posX - this.sim.entities.get(i).posX)) + ((this.posY - this.sim.entities.get(i).posY) * (this.posY - this.sim.entities.get(i).posY)));
+			if (this.sim.entities.get(i) != this && d < this.desiredSeparation && this.sim.entities.get(i).getClass().getName().equals("sim.Prey")) {
+				diff.x = posX - this.sim.entities.get(i).posX;
+				diff.y = posY - this.sim.entities.get(i).posY;
+				diff.x /= d;
+				diff.y /= d;
+				avgPos.x += posX + diff.x;
+				avgPos.y += posY + diff.y;
+				total++;
+			}
+		}
+		if (total > 0) {
+			avgPos.x /= total;
+			avgPos.y /= total;
+			a = avgPos.x - posX;
+			b = avgPos.y - posY;
+			if (a > 0) avgDir = atan(b / a);
+			else if (a < 0) avgDir = atan((b / a)) + Math.PI;
+			avgDir -= dir;
+
+			if (avgDir > turnRate) avgDir = turnRate;
+			else if (avgDir < -turnRate) avgDir = -turnRate;
+
+		}
+
+
+		return avgDir;
+	}
 }
